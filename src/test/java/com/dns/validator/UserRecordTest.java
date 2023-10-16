b2ec77312ef0097764a847104139b2b92edf74d5
Diego Núñez Silva <diego.nunez.mx@gmail.com>
2023-10-16 16:43:06 +0200

    Add deserialization test for record

--- src/test/java/com/dns/validator/UserRecordTest.java
+++ src/test/java/com/dns/validator/UserRecordTest.java
@@ -1,15 +1,35 @@
 package com.dns.validator;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class UserRecordTest {
 
+    private ObjectMapper objectMapper;
+
+    @BeforeEach
+    void setup() {
+        objectMapper = new ObjectMapper();
+    }
+
     @Test
     void createSuccess() {
         assertDoesNotThrow(() -> new UserRecord("username123", "password123", "user@email.com"));
+    }
+
+    @Test
+    void deserializeSuccess() throws JsonProcessingException {
+        var expectedUser = new UserRecord("username123", "password123", "user@email.com");
+        var user = objectMapper.readValue("""
+                { "username": "username123", "password": "password123", "email": "user@email.com" }
+                """, UserRecord.class);
+
+        assertEquals(expectedUser, user);
     }
 
     @Test
